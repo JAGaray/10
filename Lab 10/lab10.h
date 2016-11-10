8ed52f4e2ccd5e9badfe0634824130d7fcb54df0
JAGaray <jgaray0817@gmail.com>
2016-11-09 22:43:43 -0500

    Update lab10.h

--- Lab 10/lab10.h
+++ Lab 10/lab10.h
@@ -1,17 +1,26 @@
-struct Country 
-{
-  string name;
-  double pop1950;
-  double pop1970;
-  double pop1990;
-  double pop2010;
-  double pop2015;
+const int MAX_COUNTRIES = 500;
+
+// structure and class definitions
+struct Country {
+	string name; // name of country
+	// popululation of country for each specific year
+	long double pop1950;
+	long double pop1970;
+	long double pop1990;
+	long double pop2010;
+	long double pop2015;
+
+	// store growth rates for each country
+	long double growth1950to1970;
+	long double growth1970to1990;
+	long double growth1990to2010;
+	long double growth2010to2015;
+
 };
 
-struct World
-{
-  int numCountries;
-  Country countries[MAXCOUNTRIES];
-} 
-
-myWorld;
+struct World {
+	int numCountries;
+	Country countries[MAX_COUNTRIES];
+	// indices of 3 largest countries
+	int first, second, third;
+}; myWorld; // myWorld is an instance of World
